48dc39cbfd406f7ebafbdcce4754846eb39e6c4e
Naimur Rahman Sourov <130272535+rahman-naimur@users.noreply.github.com>
2023-10-23 14:14:50 +0600

    Add files via upload

---
+++ sumation of two numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("Input one number:");
+    scanf("%d", &a);
+    printf("Input another number:");
+    scanf("%d", &b);
+    sum=a+b;
+    printf("sum is %d", sum);
+    getch();
+    return 0;
+}
